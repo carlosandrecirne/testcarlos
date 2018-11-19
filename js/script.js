00ece0d108c1f96f65f11869cd17829f93646faf
Carlos Cirne <carloscirne@MacBook-Pro-de-Carlos-3.local>
2018-11-19 10:19:58 -0300

    S5- Apply layout

--- js/script.js
+++ js/script.js
@@ -4,18 +4,43 @@ function openpage(pg){
 	$(".content").hide();
 	if (pg == 'btn_map'){
 		$("#content_map").show();
+		getpoints();
 	}else if (pg == 'btn_list'){
 		$("#content_list").show();
+		getpoints();
 	}
 }
 
  var map;
  
-  function initMap() {
-	  alert('x');
+  function initMap() { 
+  var x=window.innerHeight-180; 
+  $("#content_map").css({"min-height":x,height:x});
 	map = new google.maps.Map(document.getElementById('content_map'), {
 	  center: {lat: -34.397, lng: 150.644},
 	  zoom: 8
 	});
+	
   }
+  
+  function getpoints(){
+	    initMap();
+		var s="<ul>";
+		$.get('https://raw.githubusercontent.com/carlosandrecirne/testcarlos/master/db.json',{},function(data){
+			$.each(data,function(index,value){
+				s+="<li>"+value.name+"<br>Lat:"+value.lat+"<br>Lng:"+value.lng+"</li>";
+				var marker = new google.maps.Marker({
+					position: {lat: value.lat, lng: value.lng},
+					map: map,
+					title: value.name
+				  });
+			});
+			s+="</ul>";
+			$("#content_list").html(s);
+		},'json');
+  }
+  
+  setTimeout(function(){
+	  getpoints();
+  },1000);
   
