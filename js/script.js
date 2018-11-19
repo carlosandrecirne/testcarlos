f19bace5f5e446aada908df636c81e1df3493c69
Carlos Cirne <carloscirne@MacBook-Pro-de-Carlos-3.local>
2018-11-19 09:52:27 -0300

    S3- Apply layout

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ function openpage(pg){
 		$("#content_list").show();
 	}
 }
+
+ var map;
+ 
+  function initMap() {
+	  alert('x');
+	map = new google.maps.Map(document.getElementById('content_map'), {
+	  center: {lat: -34.397, lng: 150.644},
+	  zoom: 8
+	});
+  }
+  
